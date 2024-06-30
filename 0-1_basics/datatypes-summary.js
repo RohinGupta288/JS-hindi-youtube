dccd49caf41e7016a391f18c4d59696de18594d0
Rohin Gupta <156818912+RohinGupta288@users.noreply.github.com>
2024-06-30 20:19:23 +0000

    datatypes Summary

---
+++ 0-1_basics/datatypes-summary.js
@@ -0,0 +1,25 @@
+/*primitive and non-primitive datatypes
+on the basis of how they are stored and accessed from the memory
+*/
+//Primitive(call by value)
+
+// 7 types : String, Number, Boolean, null(empty but not 0 or empty string), undefined(variable and memory space is declared but we dont know what is value)
+//           Symbol, BigInt
+
+
+//Non-Primitive(refernec type)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "rohin",
+    age: 22,
+}
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+
+
+
