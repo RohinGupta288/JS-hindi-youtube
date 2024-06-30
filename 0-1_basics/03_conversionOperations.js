de29f389a8951c41eb4ef88a4b6f219e0266411a
Rohin Gupta <156818912+RohinGupta288@users.noreply.github.com>
2024-06-30 10:54:40 +0000

    Conversion Operations

--- 0-1_basics/03_conversionOperations.js
+++ 0-1_basics/03_conversionOperations.js
@@ -1,10 +1,27 @@
-let score ="33";
-console.log(typeof score);
-console.log(typeof (score));
+let score =true;
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber=Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
+/*
+"33"->33
+"33abc"->NaN(Not a Number)
+"Rohin"->NaN
+true->1 and false->0
+*/
+let isLoggedIn="rohin";
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
 
+/*
+1->true
+3->true
+0->false
+""->false
+"rohin"->true
+*/
 
