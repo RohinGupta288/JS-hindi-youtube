3e5b5915b16633e6da339a84b05ffb419970a5ac
Rohin Gupta <156818912+RohinGupta288@users.noreply.github.com>
2024-06-30 10:38:13 +0000

    Commit Message

---
+++ 0-1_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all the JS code as newer version of JS
+
+//alert(3+3); this wont work as this is Nodejs and not browser.
+
+console.log(3+3); //this will print the number value 6
+
+let name="Rohin"
+let age=18
+let loggedIn=false;
+
+//number-> 2 to the pwr 53
+//BIGINT-> normally we dont use this
+//string=""(we can use both single and ouble quotes, but generally double is used)
+//boolean->true/false
+//null->object
+//undefined->undefined
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof age);
