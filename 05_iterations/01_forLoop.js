9055baa7fb475a8bffde98cf1bc1c3e49e153e42
Rohin Gupta <156818912+RohinGupta288@users.noreply.github.com>
2025-01-29 08:57:21 +0000

    iterations

---
+++ 05_iterations/01_forLoop.js
@@ -0,0 +1,6 @@
+//for loop
+
+for(let i = 0;i<10;i++){
+    console.log(i, end = '='); //by default har iteration new line pe jayegi aur end = '=' means
+                               // har i ke ek space baad ek '=' aa jayega
+}
---
+++ 05_iterations/02_whileLoop.js
@@ -0,0 +1,13 @@
+//while loops
+
+let id = 0
+while(id < 10){
+    console.log(id++);
+    
+}
+id =0
+do{
+    console.log(id);
+    id++;
+}while(id <= 10)
+
